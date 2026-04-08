d710b0c7118c0686b78bf0b7cb4bd31a6c772e55
laura-portela <lauraportela@id.uff.br>
2026-04-08 12:13:53 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,8 @@
+# ETAPA 1: Leitura do banco de dados
+dados = read.csv2("C:/Users/laura/Downloads/imoveis.csv")
+
+# ETAPA 2: Gráficos
+# (será desenvolvido depois)
+
+# ETAPA 3: Medidas
+# (será desenvolvido depois)
